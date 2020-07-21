e557314d06b1bba00601a76a838fc281073b5953
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-07-21 15:55:03 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -2,11 +2,11 @@
 #include <gtest/gtest.h>
  
 TEST(VitalsTest, BPM) { 
-    ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
+    ASSERT_EQ(false, vitalsAreOk(100, 100, 50));
 }
  
 TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
+    ASSERT_EQ(true, vitalsAreOk(100, 40, 50));
 }
  
 int main(int argc, char **argv) {
