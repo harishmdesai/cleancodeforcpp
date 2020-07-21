e87b1df754a52c73100a96f3c099fcf173e59f5f
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-07-21 21:46:04 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,3 +1,6 @@
+#define Minbpm 70
+#define Maxbpm 150
+
 bool IsInRange(float Value, int MinVal, int MaxVal)
 {
    return(Value<MinVal&&Value>MaxVal); 
@@ -6,5 +9,5 @@ bool IsInRange(float Value, int MinVal, int MaxVal)
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
 {
-  return ((IsInRange(bpm,70,150))||(IsInRange(spo2,80,100))||(IsInRange(respRate,30,60)));
+  return ((IsInRange(bpm,Minbpm,Maxbpm))||(IsInRange(spo2,80,100))||(IsInRange(respRate,30,60)));
 }
