8d1528a0d4ba06012928fd251db0683c98d06014
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-07-21 21:16:09 +0530

    solved compilation issues

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,10 +1,10 @@
 bool IsInRange(float Value, int MinVal, int MaxVal)
 {
-   return(Value<MinVa&&Value>MaxVal); 
+   return(Value<MinVal&&Value>MaxVal); 
 }
 
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
 {
-  return ((IsInRange(bpm,70,150))||(IsInRange(spo2,,80,100))||(IsInRange(respRate,,30,60)));
+  return ((IsInRange(bpm,70,150))||(IsInRange(spo2,80,100))||(IsInRange(respRate,30,60)));
 }
