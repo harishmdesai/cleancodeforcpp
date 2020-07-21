6298cce949a34b6c58e5d8a173bb20afe13a8da2
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-07-21 21:13:37 +0530

    Function optimization

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,20 +1,10 @@
-bool IsInRange(float Value, float MinVal, float MaxVal){
-   return(Value<MinVal||Value>MaxVal); 
-}
-
-bool IsbpmOk(float bpm){
-  return(IsInRange(bpm,70,150));
-}
-
-bool Isspo2InRange(float spo2){
-  return(IsInRange(spo2,80,100));
-}
-
-bool IsrespRateinRange(float respRate){
-  return(IsInRange(respRate,30,60); 
+bool IsInRange(float Value, int MinVal, int MaxVal)
+{
+   return(Value<MinVa&&Value>MaxVal); 
 }
 
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (IsbpmOk(bpm)||Isspo2InRange(spo2)||IsrespRateinRange(respRate));
+bool vitalsAreOk(float bpm, float spo2, float respRate) 
+{
+  return ((IsInRange(bpm,70,150))||(IsInRange(spo2,,80,100))||(IsInRange(respRate,,30,60)));
 }
