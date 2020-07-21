bf479a90aeff6f139314e91bde443a92ec30b6ac
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-07-21 15:50:52 +0530

    Fix to resolve cyclomatic omplexity check

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,17 @@
 
+bool bpmokfun(float bpm){
+  return(bpm<70|| bpm>150);
+}
+
+bool spo2fun(float spo2){
+  return(spo2<80);
+}
+
+bool respRatefun(float respRate){
+  return(respRate<30|| respRate>60); 
+}
+
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if((bpm < 70) || (bpm > 150)) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if((respRate < 30) || (respRate > 60)) {
-    return false;
-  }
-  return true;
+  return (bpmokfun(bpm)||spo2fun(spo2)||respRatefun(respRate));
 }
